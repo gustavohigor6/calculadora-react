7e2aeffc86fc96467c16067aa99ded964e9db574
Gustavo Câmara <gustavohigor6@gmail.com>
2022-09-13 19:37:38 -0300

    Init

---
+++ Documentos/calculadora/src/calculadora.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './calculadora.css';
+
+function Calculadora() {
+  return (
+    <div class="jumbotron" style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px'
+    }}>
+    </div>
+  );
+}
+
+export default Calculadora;
